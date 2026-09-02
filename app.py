9cee9977d9f59a098c5acabb2c85b753cfeb1d07
suryanpillai <34960370+suryanpillai@users.noreply.github.com>
2026-09-02 21:36:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ return {
     ),
    "reversed_text": text[::-1],
 "title_case_text": text.title(),
-    "digit_count": digit_count
+    "digit_count": digit_count,
+    "uppercase_count": uppercase_count
 }
 ```
 
