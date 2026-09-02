2e7add3b69d8ac4d426e4afa12db500f29e380b1
suryanpillai <34960370+suryanpillai@users.noreply.github.com>
2026-09-02 21:52:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 def clean_text(text):
-    return " ".join(text.split())
+return " ".join(text.split())
+
 def to_lowercase(text):
-    return text.lower()
-    def to_uppercase(text):
-    return text.upper()
+return text.lower()
+
+def to_uppercase(text):
+return text.upper()
